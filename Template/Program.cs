a209c95e96de2e42fd5b1edb646da5248e436b72
Nolan Stidham <nolan.stidham@faithlife.com>
2023-12-05 16:57:24 -0800

    Create Template project

---
+++ Template/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+	public static void Main()
+	{
+		try
+		{
+			using var inputFile = new StreamReader("../../../input.csv");
+
+			var results = new List<int>();
+			var nextLine = inputFile.ReadLine();
+			while (nextLine is not null)
+			{
+				results.Add(ProccessInputLine(nextLine));
+				nextLine = inputFile.ReadLine();
+			}
+			
+			Console.WriteLine($"RESULT: {results.Sum()}");
+		}
+		catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
+	}
+
+	private static int ProccessInputLine(string inputLine) { return 0; }
+}
